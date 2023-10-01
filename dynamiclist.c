2d38e42e65365b0f462b0e5ad71b3a881ae7038b
sssong5252 <luckyjude@naver.com>
2023-10-01 10:56:53 +0900

    fix&plan

--- dynamiclist.c
+++ dynamiclist.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
+#include <stdio.h> //입력받은 갯수만큼 정수 저장
 #include <stdlib.h>
 
 int main() {
-    int *array;
-    int n;
-    int i;
+    int *array; //포인터변수
+    int n; //배열의 크기 저장
+    int i; //반복문
 
-    printf("요소의 갯수를 입력하세요: ");
+    printf("요소의 갯수를 입력하세요: "); //정수값 입력
     scanf("%d", &n);
 
-    array = (*int) malloc(n * sizeof(int));
+    array = (*int/*반횐된 주소값 정수형 포인터 캐스팅*/) malloc(n * sizeof(int)/*필요한 메모리 공간 크기*/); //malloc: 동적으로 메모리 공간 할당
 
-    if(array == NULL) {
-        printf("메모리가 할당되지 않았습니다");
+    if(array == NULL) { //메모리 할당 실패
+        printf("메모리가 할당되지 않았습니다"); //메모리 할당 실패 == malloc이 null반환
         return 0;
     }
 
-    for(i = 0; i < n; i++){
+    for(i = 0; i < n; i++){ //0부터 n-1 까지
         array[i] = i + 1;
     }
-
-    for(i = 0; i > n; i++){
-        array[i] = i;
-    }
-
-    
+    //배열값 출력
+    //free함수 == 할당된 메모리 공간 해제 == 메모리 누수 방지 == malloc이랑 짝처럼 다님
 }
