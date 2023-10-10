824ffa43ebeb0b2d6965a7b4f48f33c92cfe4022
sssong5252 <luckyjude@naver.com>
2023-10-10 16:25:32 +0900

    tlqkf

---
+++ 1010.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+// int main()
+// {
+//     int i;
+//     char str[4];
+//     str[0] = 'a';
+//     str[1] = 'b';
+//     str[2] = 'c';
+//     str[3] = '\0';
+
+//     i = 0;
+//     while(str[i] != '\0') {
+//         printf("%c",str[i]);
+//         i++;
+//     }
+//     printf("\n");
+
+//     return 0;
+// }
+// int main()
+// {
+//     int i = 10;
+//     char c = 69;
+//     float f = 78;
+
+//     printf("i의 주소: %u\n", &i);
+//     printf("c의 주소: %u\n", &c);
+//     printf("f의 주소: %u\n", &f);
+// }
+// int main() {
+//     int i = 10;
+//     int *p;
+//     p = &i;
+
+//     printf("%u, %u\n", p, &i);
+// }
+
+// int main()
+// {
+//     char *qw;
+
+//     qw = (char *)9000;
+//     printf("%d\n", qw);
+
+//     qw++;
+//     printf("%d\n", qw);
+// }
+
+int main()
+{
+    int  v = 10;
+    int *p;
+    p = &v;
+    v = *p++;
+    printf("%d\n", v);
+    p = &v;
+    v = (*p)++;
+    printf("%d\n", v);
+    p = &v;
+    v = *++p;
+    printf("%d\n",v);
+    p = &v;
+    v = ++*p;
+    printf("%d\n",v);
+}
