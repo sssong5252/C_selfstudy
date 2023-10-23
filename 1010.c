824cbd676c24b9eff7e4790d4ea6aa73e5c78245
sssong5252 <luckyjude@naver.com>
2023-10-24 03:10:32 +0900

    0

--- 1010.c
+++ 1010.c
@@ -63,3 +63,11 @@
 //     v = ++*p;
 //     printf("%d\n",v);
 // }
+// #include <stdio.h>
+// int main(void)
+// {
+// int ch; // 정수형에 주의
+// while( (ch = getchar()) != EOF )
+// putchar(ch);
+// return 0;
+// }
